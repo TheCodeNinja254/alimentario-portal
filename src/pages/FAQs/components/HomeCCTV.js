7eab400d8ab017e4e9af4305e3e61162fc058150
fredrick <fmmbugua@safaricom.co.ke>
2021-10-08 09:13:58 +0300

    Home CCTV and 4G Router FAQs

--- src/pages/FAQs/components/HomeCCTV.js
+++ src/pages/FAQs/components/HomeCCTV.js
@@ -21,46 +21,61 @@ const useStyles = makeStyles((theme) => ({
 
 const faqs = [
   {
-    question: "What is Internet For Business?",
+    question: "What is Safaricom Home CCTV?",
     answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "It is a service that allows Safaricom Home customers to make sure that their families and their homes are protected by enabling them to be in control of the activities in their home from wherever they are.",
   },
   {
-    question: "What packages can I subscribe to?",
+    question: "How does it work?",
     answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "Safaricom Home CCTV allows you to have the ability to view the activities of your household at all times of the day from wherever you are using any of your devices.",
+  },
+  {
+    question: "Who is eligible for this product?",
+    answer: "Any Safaricom Home customer is eligible.",
+  },
+  {
+    question: "How can I access the service?",
+    answer:
+      "You can access it by purchasing a surveillance camera from any Safaricom shops or ordering one from Masoko online store. If the device is ordered from Masoko, it will be delivered to you within 72 hours. You will also be required to have a strong and sustainable Internet connection in your home.",
+  },
+  {
+    question: "How do I install the surveillance camera?",
+    answer:
+      "Each device comes with an installation manual to assist you to install the device. If you would like to have the device installed, you should download the Safaricom Home App and request for installation under the 'Get Help Tab'.",
+  },
+  {
+    question: "How much is the Safaricom Home CCTV service?",
+    answer:
+      "You will be charged a one-time cost to purchase the camera. Afterwards, the service is free.",
   },
   {
     question:
-      "WWhat do I require to sign up for Internet for Business service?",
-    answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "Can I purchase the Safaricom Home CCTV service without a Safaricom Home Fiber package?",
+    answer: "Yes, you can.",
   },
   {
-    question: "Where can I get this service?",
+    question: "How can I get support if my camera is not working correctly?",
     answer:
-      "Sample answer provided here. More information to be populated on availability.",
-  },
-  {
-    question: "What equipment is installed at the customer premise?",
-    answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "Simply call us at 400. If your problem cannot be solved, you will be requested to bring in your device to a Safaricom shop for further assistance.",
   },
   {
     question:
-      "How long will it take before Internet for Business service is installed at my premise?",
+      "If I wish to opt-out of the service will I be refunded for my camera?",
     answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "No, you will not be refunded for your device if you no longer wish to use it.",
   },
   {
-    question: "Is there any installation fees?",
+    question: "What do I do if the camera is not working correctly?",
     answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "Kindly check to see if the camera is receiving enough bandwidth from your router. " +
+      "If not, kindly purchase a device that goes by the name, 'Internet repeater' from a Safaricom shop to boost your Internet connectivity to where you have placed the camera.",
   },
   {
-    question: "What do I do when I need support for my Connectivity service?",
+    question:
+      "How will I view the activities of my home using the surveillance camera?",
     answer:
-      "Sample answer provided here. More information to be populated on availability.",
+      "Simply download the app for the specific device you are using from the App Store or Google Play store and set up the application with the instructions on the device manual. Once done, you should be able to view your home’s activities.",
   },
 ];
 

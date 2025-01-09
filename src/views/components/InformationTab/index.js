cdc51af82e37af18d785a1176a1a2d0b47d4903c
fredrick <m.mwangi.fredrick@gmail.com>
2025-01-09 23:19:46 +0300

    Update styling on information tab

--- src/views/components/InformationTab/index.js
+++ src/views/components/InformationTab/index.js
@@ -1,5 +1,6 @@
-import { Grid } from "@material-ui/core";
+import { Grid, Box } from "@material-ui/core";
 import React from "react";
+import { useTheme } from "@material-ui/core/styles";
 import { gridSpacing } from "../../../store/constant";
 import MenuCard from "../ActionCards";
 import BannerCard from "../ActionCards/BannerCard";
@@ -10,26 +11,29 @@ import BrandsQuickLinks from "../../../components/BrandsQuickLinks";
 const InformationTab = ({ showRecentOrders = true }) => {
   const showFeature = false;
   const isLoading = false;
+  const theme = useTheme();
 
   return (
-    <Grid container spacing={gridSpacing}>
-      <BrandsQuickLinks />
-      {showRecentOrders && (
-        <Grid item xs={12}>
-          <MenuCard />
-        </Grid>
-      )}
+    <Box sx={{ flexGrow: 1, paddingX: theme.spacing(2) }}>
+      <Grid container spacing={gridSpacing}>
+        <BrandsQuickLinks />
+        {showRecentOrders && (
+          <Grid item xs={12}>
+            <MenuCard />
+          </Grid>
+        )}
 
-      <Grid item xs={12}>
-        <BannerCard />
+        <Grid item xs={12}>
+          <BannerCard />
+        </Grid>
+        <Grid item xs={12}>
+          <WeDeliverCard />
+        </Grid>
+        <Grid item xs={12}>
+          {showFeature && <PopularCard isLoading={isLoading} />}
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <WeDeliverCard />
-      </Grid>
-      <Grid item xs={12}>
-        {showFeature && <PopularCard isLoading={isLoading} />}
-      </Grid>
-    </Grid>
+    </Box>
   );
 };
 
